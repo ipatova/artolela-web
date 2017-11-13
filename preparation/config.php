9370b2c95f48d5673708b9cd42eef2069c7846d4
ipatova <ipatova-julia@rambler.ru>
2017-11-14 00:09:52 +0300

    Create config.php

---
+++ preparation/config.php
@@ -0,0 +1,6 @@
+<?php
+    $login = "your login";      #login from server
+    $password = "your password";   #password from server
+    $db = "your database";#название созданной базы данных
+    
+?>
