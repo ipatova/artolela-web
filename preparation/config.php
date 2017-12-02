47b6085644567586474d8952f39d7adbfad4f0c5
ipatova <ipatova-julia@rambler.ru>
2017-12-02 18:38:58 +0300

    Delete config.php

--- preparation/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $login = "snowwarrio";      #login from server
-    $password = "tainexahiw";   #password from server
-    $db = "snowwarrio_pictures";#название созданной базы данных
-    
-    $link = mysqli_connect("localhost", $login, $password, $db);
-?>
